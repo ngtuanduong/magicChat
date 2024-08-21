8ad46bac077ea1cc8fb0f7451aec3108ed1be2f9
ntduong <94307861+ngtuanduong@users.noreply.github.com>
2024-08-21 13:56:22 +0700

    Update script.js

--- public/script.js
+++ public/script.js
@@ -1,5 +1,5 @@
-// const socket = io("https://magichat.onrender.com/");
-const socket = io("http://localhost:3000/")
+const socket = io("https://magichat.onrender.com/");
+// const socket = io("http://localhost:3000/")
 
 function displayImage() {
     var input = document.getElementById('fileInput');
